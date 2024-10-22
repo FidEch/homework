b3c42219753f8fcc064ea6486c5583160f3f8948
FidEch <fidelechevarr@gmail.com>
2024-10-22 12:35:06 +0300

    created 22/10/24

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var= 1.5, 1, 2 , 3 , True, False, "one"
+print(immutable_var)
+#immutable_var[0]=1 - нельзя редактировать или изменять данные кортежа.
+mutable_list= [1.5, 1, 2 , 3 , True, False, "one"]
+mutable_list[0]=1
+mutable_list[5]=True
+print(mutable_list)
