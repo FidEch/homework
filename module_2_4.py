752cdff1c8419e990a19b195c8d41b7cb94f1fed
FidEch <fidelechevarr@gmail.com>
2024-11-02 21:32:24 +0300

    02/11/2024

---
+++ module_2_4.py
@@ -0,0 +1,20 @@
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+prime = []
+not_prime = []
+is_prime = True
+numbers.remove(1)
+for i in numbers:
+  k = 0
+  for j in range(2, i // 2+1):
+    if (i % j == 0):
+        k = k+1
+  if (k <= 0):
+    is_prime = True
+    prime.append(i)
+  else:
+    is_prime = False
+    not_prime.append(i)
+print("Число простое: ", prime)
+print("Число не является простым: ", not_prime)
+
+
