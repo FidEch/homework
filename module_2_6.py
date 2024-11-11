165a28d339d9ef6c68ef6fb0eb3a05d00694d43c
FidEch <fidelechevarr@gmail.com>
2024-11-12 00:01:50 +0300

    11/11/2024

--- module_2_6.py
+++ module_2_6.py
@@ -1,11 +1,12 @@
 def the_gate ():
-    gate_1 = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    gate_2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    n = int(input('Enter n: '))
     password = []
-    print(type(gate_1))
-    print(type(gate_2))
-    for i in range(len(gate_2)+1):
-        for j in range(len(gate_2)+1):
-            if i == 0 or j == 0:
-                return password.append(i,j)
-            print(password)
+    for i in range(n):
+        for j in range(n):
+            if i+j == n:
+                if j > i:
+                    password.append({i,j})
+                if j < i:
+                    continue
+    print(password)
+the_gate()
