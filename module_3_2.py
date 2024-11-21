81acb60fee07a32a4f4bd173f6d68951cf214968
FidEch <fidelechevarr@gmail.com>
2024-11-22 00:16:33 +0300

    22/11/2024

--- module_3_2.py
+++ module_3_2.py
@@ -1,15 +1,17 @@
 def send_email (message, recipient, sender = "university.help@gmail.com"):
-    if "@" and ".ru" not in recipient:
-        print( "Невозможно отправить письмо с адреса ", sender, "на адрес ",recipient,".")
+    eding = (".com", ".ru", ".net")
+    if "university.help@gmail.com" == sender:
+        return print("Письмо успешно отправлено с адреса ", sender, "на адрес ", recipient)
     elif sender == recipient:
-        print("Нельзя отправить письмо самому себе!")
-    elif "university.help@gmail.com" == sender:
-        print("Письмо успешно отправлено с адреса ", sender, "на адрес ", recipient,".")
+        return print("Нельзя отправить письмо самому себе!")
+    elif sender.endswith(eding) and "@" in sender != sender.endswith(eding) and "@" in sender:
+        return print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
     else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient,".")
+        return print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.ru')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
            sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.com', sender='urban.teacher@mail.uk')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
