986ca3616131c44d03a3ab4a0c897958e7815fc5
FidEch <fidelechevarr@gmail.com>
2024-11-29 21:53:17 +0300

    29/11/2024

--- module_5_1.py
+++ module_5_1.py
@@ -7,10 +7,14 @@ class House:
             print('Такого этажа не существует')
         else:
             print(*list(range(1,new_floor + 1)), sep = '\n')
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
-
-
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
