01cfd09c461c10cbe0b9eccde377372e14002e3c
FidEch <fidelechevarr@gmail.com>
2024-12-22 21:45:08 +0300

    22/12/2024

---
+++ module_7_1.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category =category
+    def __str__(self):
+        return f'Название: {self.name}, вес: {self.weight}, категория: {self.category}'
+class Shop():
+    __file_name = 'products.txt'
+    file = open(__file_name, 'r')
+    pprint(file.read())
+    file.close()
+
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+print(p2) # __str__
+#s1.add(p1, p2, p3)
